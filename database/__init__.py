13a98e201ec9323d85faf600593fb2b18adeecba
link9c <link9chen@gmail.com>
2020-06-12 11:10:03 +0800

    change

--- database/__init__.py
+++ database/__init__.py
@@ -1,20 +1,14 @@
 import pymysql
 import aiomysql
+from secret import *
 
-# conn = pymysql.connect(host='127.0.0.1', user='root', password='root', database='coolq', port=3307)
-# remote_conn = pymysql.connect(
-#     host="118.xxx.209",
-#     port=3306,
-#     user="link",
-#     password="xxxx",
-#     db="coolq")
 
 async def connect_mysql():
     return await aiomysql.connect(
-        # host="118.xxx.209",
+        # host=HOST,
         host="127.0.0.1",
         port=3306,
-        user="link",
-        password="xxxx",
+        user=USER,
+        password=MYSQLPASS,
         db="coolq"
     )
