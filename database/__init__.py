dd2cb04a5197bf99b3d043d2709f93a5b056ad86
link9c <link9chen@gmail.com>
2020-06-12 09:37:33 +0800

    delete pass

--- database/__init__.py
+++ database/__init__.py
@@ -3,18 +3,18 @@ import aiomysql
 
 # conn = pymysql.connect(host='127.0.0.1', user='root', password='root', database='coolq', port=3307)
 # remote_conn = pymysql.connect(
-#     host="118.25.135.209",
+#     host="118.xxx.209",
 #     port=3306,
 #     user="link",
-#     password="12345",
+#     password="xxxx",
 #     db="coolq")
 
 async def connect_mysql():
     return await aiomysql.connect(
-        # host="118.25.135.209",
+        # host="118.xxx.209",
         host="127.0.0.1",
         port=3306,
         user="link",
-        password="12345",
+        password="xxxx",
         db="coolq"
     )
