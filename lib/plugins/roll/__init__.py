7c22e6b3e0404a79f2670443e9a9d57765f7eff1
Your Name <you@example.com>
2020-06-14 16:18:31 +0800

    'changeruler'

--- lib/plugins/roll/__init__.py
+++ lib/plugins/roll/__init__.py
@@ -9,38 +9,32 @@ async def roll(session: CommandSession):
     if session.ctx.get('preprocessed'):
         text = session.current_arg_text.strip()
         # 3D100+8/9
-        print(text)
-        if text:
-            _text = text.split('d')
-            roll_map = []
-            offset_map = []
-            if len(_text) == 2 and _text[0].isdigit():
-                text_offset = _text[1]
-                # regx = re.findall(r'(\d+)([+\-*/])(\d+)', text_offset)
-                regx = re.findall(r'(\d+)([+\-*/>])', text_offset)
-                print(regx)
-                if regx and regx[0][0].isdigit() and len(regx[0]) == 2:
+        # (1d100+10-5-4-3) * 2
+        roll_map = []
+        offset_map = []
+        regx = re.findall('(\d+)d(\d+)', text)
+        if regx != [] and len(regx) == 1:
+            turns = int(regx[0][0])
+            random_range = int(regx[0][1])
+            ruler = "%sd%s" % (turns, random_range)
+            sign = re.findall('[+\-*/]', text) != []
+            if len(regx[0]) == 2 and sign:
+                for i in range(turns):
+                    rolled = random.randint(1, random_range)
+                    try:
+                        offset = eval(text.replace(ruler, str(rolled)))
+                    except:
+                        return
+                    result = int(round(offset, 0)) if offset > 0 else 0
+                    roll_map.append(rolled)
+                    offset_map.append(result)
+                back_text = "%s %s 偏移后%s 总计%s" % (
+                    text.strip(), str(roll_map), str(offset_map), sum(offset_map))
+            else:
 
-                    a = int(_text[0])
-                    b = int(regx[0][0])
+                for i in range(turns):
+                    rolled = random.randint(1, random_range)
+                    roll_map.append(rolled)
 
-                    for i in range(a):
-                        rolled = random.randint(1, b)
-                        try:
-                            offset = eval(text_offset.replace(str(b), str(rolled)))
-                        except:
-                            return
-                        result = int(round(offset, 0)) if offset > 0 else 0
-                        roll_map.append(rolled)
-                        offset_map.append(result)
-                    back_text = "%s %s 偏移后%s 总计%s" % (
-                        text.strip(), str(roll_map), str(offset_map), sum(offset_map))
-                else:
-                    a = int(_text[0])
-                    b = int(regx[0][0])
-                    for i in range(a):
-                        rolled = random.randint(1, b)
-                        roll_map.append(rolled)
-
-                    back_text = "%s %s 总计%s" % (text.strip(), str(roll_map), sum(roll_map))
-                await session.send(back_text, at_sender=True)
+                back_text = "%s %s 总计%s" % (text.strip(), str(roll_map), sum(roll_map))
+            await session.send(back_text, at_sender=True)
